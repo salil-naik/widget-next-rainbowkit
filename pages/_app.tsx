b96405a8e89351bfa4b121c0e753c39528613b7d
salil-naik <salil.naik27@gmail.com>
2023-08-04 07:31:39 +0530

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import '../styles/globals.css';
-import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import type { AppProps } from 'next/app';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import type { AppProps } from "next/app";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import {
   arbitrum,
   goerli,
@@ -11,10 +11,26 @@ import {
   polygon,
   zora,
   zkSync,
-  base
-} from 'wagmi/chains';
-import { publicProvider } from 'wagmi/providers/public';
-
+} from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+const base = {
+  id: 8453,
+  name: "Base Mainnet",
+  network: "base",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ether",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    public: { http: ["https://mainnet.base.org"] },
+    default: { http: ["https://mainnet.base.org"] },
+  },
+  blockExplorers: {
+    etherscan: { name: "BaseScan", url: "https://basescan.org/" },
+    default: { name: "BaseScan", url: "https://basescan.org/" },
+  },
+};
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [
     mainnet,
@@ -24,14 +40,14 @@ const { chains, publicClient, webSocketPublicClient } = configureChains(
     zora,
     zkSync,
     base,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [goerli] : []),
   ],
   [publicProvider()]
 );
 
 const { connectors } = getDefaultWallets({
-  appName: 'RainbowKit App',
-  projectId: '98d93622dc0f4cfb95dc432769be24b5',
+  appName: "RainbowKit App",
+  projectId: "98d93622dc0f4cfb95dc432769be24b5",
   chains,
 });
 
